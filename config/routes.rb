7aa2c725e8067ea08955a927cbe5169d6c7ee060
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2017-05-07 17:16:41 +0200

    Localize routes by default.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Ecm::Courses::Engine.routes.draw do
   localized do
-    resources :course_categories, only: [:index, :show]
-    resources :courses, only: [:show] do
-      member do
-        get "calendar", action: :calendar
-        get "calendar/:year/:month", action: :calendar
+    scope :ecm_courses do
+      resources :course_categories, only: [:index, :show]
+      resources :courses, only: [:show] do
+        member do
+          get "calendar", action: :calendar
+          get "calendar/:year/:month", action: :calendar
+        end
       end
+      resources :course_dates, only: [:show]
+      
+      root to: 'course_categories#index'
     end
-    resources :course_dates, only: [:show]
   end
 end
